e809c36244a4e35d1ca0600e6c16ee50dcbfcd5e
chris_mlvz <chris.mlvz@gmail.com>
2021-01-04 20:23:58 -0600

    Code

---
+++ dibujandoFlechas/eventos.js
@@ -0,0 +1,55 @@
+
+document.addEventListener("keydown", dibujarTeclado)
+var cuadroDibujo = document.getElementById("areaDibujo")
+var papel = cuadroDibujo.getContext("2d")
+
+var teclas =
+{
+    LEFT : 37,
+    UP : 38,
+    RIGHT : 39,
+    DOWN : 40
+};
+var x = 100;
+var y = 100;
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3
+    lienzo.moveTo(xinicial,yinicial);
+    lienzo.lineTo(xfinal,yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujarTeclado(evento)
+{   
+    var colorLinea = "red"
+    var movimiento = 10;
+    switch(evento.keyCode)
+    {
+        case teclas.UP:
+            dibujarLinea(colorLinea,x,y,x,y-movimiento,papel);
+            y = y - movimiento
+        break; 
+        case teclas.DOWN:
+            dibujarLinea(colorLinea,x,y,x,y+movimiento,papel);
+            y = y + movimiento
+        break; 
+        case teclas.LEFT:
+            dibujarLinea(colorLinea,x,y,x-movimiento,y,papel);
+            x = x - movimiento
+        break; 
+        case teclas.RIGHT:
+            dibujarLinea(colorLinea,x,y,x+movimiento,y,papel);
+            x = x + movimiento
+        break;  
+        default:
+            console.log("Solo flechas")
+        break;  
+    }
+}
+
+dibujarLinea("red", x-1 , y-1 , x+1, y+1, papel)
