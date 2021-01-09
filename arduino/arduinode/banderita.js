b41c634ca469166ef21902bbb47179f1e908394a
chris_mlvz <chris.mlvz@gmail.com>
2021-01-08 21:48:23 -0600

    Blink + MoveServo Fixed

--- arduino/arduinode/banderita.js
+++ arduino/arduinode/banderita.js
@@ -1,7 +1,8 @@
 var jf = require("johnny-five");
 var circuito = new jf.Board();
-var led;
-var motor; 
+var ledRojo;
+var ledAmarillo;
+var motor;
 var fotoCelda;
 var turno = 0;
 
@@ -9,41 +10,50 @@ circuito.on("ready", prender);
 
 function prender()
 {
-  var configuracion = 
-  {
-    pin:"A0",
-    freq: 50
-  };
-  fotoCelda = new jf.Sensor(configuracion);
+    var configuracion = 
+    {
+        pin: "A0",
+        freq: 50
+    };
+    fotoCelda = new jf.Sensor(configuracion);
 
-  led = new jf.Led(3);
-  led.on();
+    ledRojo = new jf.Led(3);
+    ledAmarillo = new jf.Led(4);
 
-  motor = new jf.Servo(8);
-  motor.to(0);
-  moverServo();
+    motor = new jf.Servo(8);
+    motor.to(0);
+
+    moverServo();
 }
 
 function moverServo()
 {
-  console.log("Luz: " + fotoCelda.value);
-  var luz = fotoCelda.value;  
-  if(luz > 800)
-  {
-    if(turno == 1)
+    console.log("Luz: " + fotoCelda.value);
+    var luz = fotoCelda.value
+    if(luz > 500)
     {
-      turno = 0;
-      motor.to(70);
+        if(turno == 1)
+        {
+            turno = 0;
+            motor.to(60);
+            ledRojo.on(500);
+            ledAmarillo.off();
+        }
+        else
+        {
+            turno = 1;
+            motor.to(120);
+            ledAmarillo.on(500);
+            ledRojo.off();
+        }
+        
     }
     else
     {
-      turno = 1;
-      motor.to(110);
+        motor.to(0);
+        ledRojo.off();
+        ledAmarillo.off();
     }
-  }
-  else
-  {
-      motor.to(150);
-  }
-  setTimeout(moverServo, 1000);
+    setTimeout(moverServo, 1000);
 }
+
